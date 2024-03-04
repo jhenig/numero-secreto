5346a163f4d974c9cc6668fdd7c57bf1e846dbc7
jhenifer Gomes <jheniferg.260@gmail.com>
2024-03-03 21:25:33 -0300

    Deixando o jogo mais facil

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 let listaDeNumerosSorteados = [];
-let numeroLimite = 100;
+let numeroLimite = 50;
 let numeroSecreto = gerarNumeroAleatorio();
 let tentativas = 1;
 
@@ -12,7 +12,7 @@ function exibirTexto(tag, texto) {
 
 function exibirMensagemInicial() {
     exibirTexto('h1', 'Jogo do número secreto');
-    exibirTexto('p', 'Escolha um número entre 1 e 10');
+    exibirTexto('p', 'Escolha um número entre 1 e 50');
 }
 
 exibirMensagemInicial()
